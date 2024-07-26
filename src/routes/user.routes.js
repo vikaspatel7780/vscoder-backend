ac1fe2e8a5b8297eca9dd6ca54bb8e0c1d79ff66
vikaspatel7780 <120778869+vikaspatel7780@users.noreply.github.com>
2024-07-26 13:15:11 +0530

    fixed error Cloudinary

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,11 +7,7 @@ const router = Router();
 router.route("/register").post(
     upload.fields([
         {
-            name:"avatar",
-            maxCount:1
-        },
-        {
-            name:"coverImage",
+            name:"image",
             maxCount:1
         }
     ]),
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,36 +1,31 @@
-import { v2 as cloudinary } from 'cloudinary'; // Cloudinary v2 ka import statement
-import fs from 'fs'; // File system module ka import
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
 
 cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // Cloudinary cloud name environment variable se
-    api_key: process.env.CLOUDINARY_API_KEY, // Cloudinary API key environment variable se
-    api_secret: process.env.CLOUDINARY_API_SECRET  // Cloudinary API secret environment variable se
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
     try {
-        if (!localFilePath) {
-            throw new Error('Local file path is missing.'); // Agar local file path missing hai, toh error throw karo
-        }
-        
+        if (!localFilePath) return null
+        //upload the file on cloudinary
         const response = await cloudinary.uploader.upload(localFilePath, {
-            resource_type: 'auto' // Auto resource type se file upload karo
-        });
-        
-        console.log('File uploaded successfully on Cloudinary:', response.secure_url); // File successfully upload hone ki confirmation
-        
-        // Successful upload ke baad local file ko delete karo
-        fs.unlinkSync(localFilePath);
-        
+            resource_type: "auto"
+        })
+        // file has been uploaded successfull
+        console.log("file is uploaded on cloudinary ", response.url);
+        fs.unlinkSync(localFilePath)
         return response;
-    } catch (error) {
-        console.error('Error uploading file to Cloudinary:', error); // Cloudinary mein file upload karne mein error
-        if (fs.existsSync(localFilePath)) {
-            fs.unlinkSync(localFilePath); // Agar error aata hai toh local file ko delete karo
-            console.log('Local file deleted.');
-        }
-        throw error;
-    }
-};
 
-export  { uploadOnCloudinary }; // uploadOnCloudinary function ko export karo
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+        return null;
+    }
+}
+
+
+
+export {uploadOnCloudinary}
